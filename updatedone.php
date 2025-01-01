29a65a0dbc823a2c6deaccb8a8cdfc05923f5d55
Gembali dileep kumar <95481217+dileepkumargembali1@users.noreply.github.com>
2025-01-01 16:38:24 +0530

    Update updatedone.php

--- updatedone.php
+++ updatedone.php
@@ -3,7 +3,6 @@
 namespace Drupal\notfoundpassthrough\Form;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Config\TypedConfigManagerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\path_alias\AliasManagerInterface;
@@ -35,20 +34,6 @@ class SettingsForm extends ConfigFormBase {
   protected $requestContext;
 
   /**
-   * The typed configuration manager.
-   *
-   * @var \Drupal\Core\Config\TypedConfigManagerInterface
-   */
-  protected $typedConfigManager;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormId() {
-    return 'notfoundpassthrough_settings';
-  }
-
-  /**
    * Constructs a SettingsForm object.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
@@ -59,22 +44,18 @@ class SettingsForm extends ConfigFormBase {
    *   The path validator.
    * @param \Drupal\Core\Routing\RequestContext $request_context
    *   The request context.
-   * @param \Drupal\Core\Config\TypedConfigManagerInterface $typed_config_manager
-   *   The typed config manager.
    */
   public function __construct(
     ConfigFactoryInterface $config_factory,
     AliasManagerInterface $alias_manager,
     PathValidatorInterface $path_validator,
-    RequestContext $request_context,
-    TypedConfigManagerInterface $typed_config_manager
+    RequestContext $request_context
   ) {
-    parent::__construct($config_factory, $typed_config_manager);
+    parent::__construct($config_factory);
 
     $this->aliasManager = $alias_manager;
     $this->pathValidator = $path_validator;
     $this->requestContext = $request_context;
-    $this->typedConfigManager = $typed_config_manager;
   }
 
   /**
@@ -85,8 +66,7 @@ class SettingsForm extends ConfigFormBase {
       $container->get('config.factory'),
       $container->get('path_alias.manager'),
       $container->get('path.validator'),
-      $container->get('router.request_context'),
-      $container->get('config.typed')
+      $container->get('router.request_context')
     );
   }
 
@@ -98,11 +78,19 @@ class SettingsForm extends ConfigFormBase {
   }
 
   /**
-   * Display Redirects 404 settings form.
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'notfoundpassthrough_settings';
+  }
+
+  /**
+   * Builds the settings form.
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $settings = $this->config('notfoundpassthrough.settings');
 
+    $form = [];
     $form['redirect_options'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Redirect options'),
@@ -111,7 +99,7 @@ class SettingsForm extends ConfigFormBase {
     $form['redirect_options']['servers'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Legacy servers'),
-      '#description' => $this->t('Enter server addresses. One server per line.'),
+      '#description' => $this->t('Enter server addresses. One per line.'),
       '#default_value' => $settings->get('servers'),
     ];
 
@@ -119,17 +107,11 @@ class SettingsForm extends ConfigFormBase {
   }
 
   /**
-   * Validates the submitted settings form.
+   * Validates the settings form.
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (!$form_state->isValueEmpty('site_404')) {
-      $form_state->setValueForElement($form['redirect_options']['site_404'], $this->aliasManager->getPathByAlias($form_state->getValue('site_404')));
-    }
     if (($value = $form_state->getValue('site_404')) && $value[0] !== '/') {
       $form_state->setErrorByName('site_404', $this->t("The path '%path' must start with a slash.", ['%path' => $value]));
-    }
-    if (!$form_state->isValueEmpty('site_404') && !$this->pathValidator->isValid($form_state->getValue('site_404'))) {
-      $form_state->setErrorByName('site_404', $this->t("The path '%path' is invalid or you do not have access to it.", ['%path' => $form_state->getValue('site_404')]));
     }
   }
 
@@ -144,4 +126,3 @@ class SettingsForm extends ConfigFormBase {
     parent::submitForm($form, $form_state);
   }
 }
-
